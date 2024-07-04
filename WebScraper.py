edfd8cc0ba22613f2d1d9ca30a2def8c0506860f
OnatGuriz <51419540+OnatGuriz@users.noreply.github.com>
2024-07-04 21:18:14 +0200

    Update to keep html format

--- WebScraper.py
+++ WebScraper.py
@@ -1,19 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
+from bs4 import BeautifulSoup
 
 #loads the driver and waits for it to load
-driver = webdriver.Firefox(GeckoDriverManager().install())
 driver = webdriver.Firefox()
 driver.implicitly_wait(2)
 
 driver.get("https://extension765.com/blogs/soderblog/seen-read-2023")
 
-#takes the text from the post_main class
-text = driver.find_element(By.ID,"post_main").text
-print(text)
+#takes the html from the post_main class
+main_post = driver.find_element(By.ID,"post_main")
+html_version =  main_post.get_attribute('outerHTML')
+
+soup = BeautifulSoup(html_version, 'html.parser')
+
+#keeps the bold and italics tags for further use
+def extract_text_with_tags(soup):
+    for tag in soup.find_all(True): 
+        if tag.name not in ['strong', 'em']:
+            tag.unwrap()  
+    return str(soup)
+
+
+rich_text = extract_text_with_tags(soup)
+
+print(rich_text)
+
+
 
 #exits the driver
 driver.close()
