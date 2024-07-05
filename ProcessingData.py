07cdcdef0b27ad45e68c8aff216880c974efe726
OnatGuriz <51419540+OnatGuriz@users.noreply.github.com>
2024-07-05 14:09:03 +0200

    Adding commas to transform into a csv

--- ProcessingData.py
+++ ProcessingData.py
@@ -1,4 +1,8 @@
 import re
+import csv
+import pandas as pd
+from io import StringIO
+
 #reads the data
 with open('Raw_data.txt', 'r', encoding='utf-8') as file:
     content = file.read()
@@ -6,10 +10,14 @@ with open('Raw_data.txt', 'r', encoding='utf-8') as file:
 #finds the dates in the expression and seperates them
 regex_dates = r'\d{2}/\d{2}'
 dates = re.findall(regex_dates, content)
-content = re.sub(regex_dates, '', content)
 
 #removes instances of data-mce-fragment and </em> or </strong>
 content = re.sub(r' data-mce-fragment="1"', '', content)
 content  = re.sub(r'</(em|strong)>', '', content)
+
+#adds a comma before dates and tags for csv
+content = re.sub(regex_dates, r'\g<0>,', content)
+regex_tags = r'<(em|strong)>'
+content = re.sub(regex_tags, r'\g<0>,', content)
 
 print(content)
