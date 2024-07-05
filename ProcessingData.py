5ead715bee6d9fd3ab814216420d462636dcfdd7
OnatGuriz <51419540+OnatGuriz@users.noreply.github.com>
2024-07-05 22:18:04 +0200

    Added the unique changes to the script

--- ProcessingData.py
+++ ProcessingData.py
@@ -1,27 +1,40 @@
 import re
 import csv
 import pandas as pd
-from io import StringIO
 
-#reads the data
-with open('Raw_data.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
+#takes text file and makes it a dataframe
+def Make_Df_From_Txt(text):
+    df = pd.DataFrame([text],columns=['Content'])
+    split_content= df.iloc[0,0].split('\n')
+    new_df = pd.DataFrame(split_content, columns=['Content'])
+    new_df = new_df.iloc[10:].reset_index(drop=True)
+    return new_df
 
-#finds the dates in the expression and seperates them
-regex_dates = r'\d{2}/\d{2}'
-dates = re.findall(regex_dates, content)
+#removes the irrelevant parts of the text file and removes the unneeded parts of the string
+def Remove_Extra_From_DF(df):
+    df["Dates"] = df["Content"].str.extract(r'(\d{2}/\d{2})')
+    df["Content"] = df["Content"].apply(lambda x: x[5:])
+    df["Content"] = df["Content"].str.replace(r' data-mce-fragment="1"', "")
+    df["Content"] = df["Content"].str.replace(r'(</em>,)[^,]*', r'\1', regex=True)
+    return df
 
-#removes instances of data-mce-fragment and </em> or </strong>
-content = re.sub(r' data-mce-fragment="1"', '', content)
-content  = re.sub(r'</(em|strong)>', '', content)
+def Split_Titles(df):
+    df = df.assign(Content = df.Content.str.split('</em>')).explode("Content")
+    df = df.assign(Content = df.Content.str.split('</strong>')).explode("Content")
+    df = df.assign(Content = df.Content.str.split(',')).explode("Content")
+    return df
 
-#adds a comma before dates and tags for csv
-content = re.sub(regex_dates, r'\g<0>,', content)
-regex_tags = r'<(em|strong)>'
-content = re.sub(regex_tags, r'\g<0>,', content)
 
-#some cases have tags some dont to make the rows match up we will add an extra comma with
+#from here we need spesific functions that automate the .apply lambda functions
 
-file = open('Content.txt', 'w', encoding='utf-8')
-file.write(content)
+#main function
+def Process_From_Raw_Data(inputfilename):
+    with open(inputfilename, 'r', encoding='utf-8') as file:
+        content = file.read()
+    df = Make_Df_From_Txt(content)
+    df = Remove_Extra_From_DF(df)
+    df = Split_Titles(df)
+    return df
 
+
+print(Process_From_Raw_Data('Raw_Data.txt'))
