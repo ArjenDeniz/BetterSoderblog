4f69b2397a0f50b9ed6821effb65ec7507b3cdf3
OnatGuriz <51419540+OnatGuriz@users.noreply.github.com>
2024-07-08 21:38:57 +0200

    fixed text file cluttering

--- Final_Script.py
+++ Final_Script.py
@@ -1,6 +1,8 @@
 from ProcessingData import Process_From_Raw_Data
 from WebScraper import Link_To_Text
+import os
 
-for year in range (2009,2023):
-    Link_To_Text(year)
+for year in range (2009,2024):
+    if os.path.exists('Raw_Data_'+ str(year) + '.txt') == False:
+        Link_To_Text(year)
     Process_From_Raw_Data(year)
